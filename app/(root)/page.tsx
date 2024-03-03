4dffc8bf0bc805fc15733744b38258f8b184628b
capricornus44 <antonzavalniuk@gmail.com>
2024-03-03 15:01:46 +0100

    wrap CategoriesFilter in Suspense component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react'
+
 import CategoriesFilter from '@/components/categories-filter'
 
 export default function Home() {
   return (
     <main className='container mx-auto px-5 lg:px-10'>
-      <CategoriesFilter />
+      <Suspense fallback={<div>Loading...</div>}>
+        <CategoriesFilter />
+      </Suspense>
     </main>
   )
 }
